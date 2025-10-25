897669b89122a796b3a4f321a0434b3a6fdb660f
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 19:23:54 -0300

    Uso de factory habilitado.

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -6,8 +6,10 @@ namespace App\Models;
 
 // Invocar o ORM Eloquent.
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tarefa extends Model
 {
+    use HasFactory;
     protected $fillable = ["titulo", "completo"];
 }

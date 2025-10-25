fe9af2a0f9a2dec6763852575eee9f262ed10a13
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 19:23:27 -0300

    Migração.

--- database/migrations/2025_10_25_221802_create_tarefas_table.php.php
+++ database/migrations/2025_10_25_221802_create_tarefas_table.php.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tarefas', function (Blueprint $table) {
+        Schema::create("tarefas", function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("titulo");
+            $table->boolean("completo");
         });
     }
 
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tarefas');
+        Schema::dropIfExists("tarefas");
     }
 };

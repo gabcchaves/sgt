e231f55352ee18d4bb0700e999e333a4f02a01d3
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 17:29:36 -0300

    Definição de uma conexão.

--- database/migrations/tarefa.php
+++ database/migrations/tarefa.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
+    protected $connection = "sqlite";
+
     // Operações de inserção.
     public function up(): void
     {

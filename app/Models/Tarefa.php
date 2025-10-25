9b75bfa0d205bb07faf4b3191ae390eccdd30b85
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 17:41:47 -0300

    Preparação preliminar do modelo da entidade Tarefa.

---
+++ app/Models/Tarefa.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Modelo para a entidade Tarefa. */
+
+namespace App\Models;
+
+class Tarefa
+{
+    public function criar(): array
+    {
+        return null;
+    }
+
+
+    public function buscar(): array
+    {
+        return null;
+    }
+
+
+    public function atualizar(): array
+    {
+        return null;
+    }
+
+
+    public function deletar(): array
+    {
+        return null;
+    }
+}

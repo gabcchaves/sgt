93f8f0f8ae13d274994d63c03172589ceb72180c
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 19:24:59 -0300

    Uso de timestamps.

--- database/migrations/2025_10_25_221802_create_tarefas_table.php.php
+++ database/migrations/2025_10_25_221802_create_tarefas_table.php.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("titulo");
             $table->boolean("completo");
+            $table->timestamps();
         });
     }
 

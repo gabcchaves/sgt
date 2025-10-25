714e5f5fd36fd5d1dddab72ab5d0f44347a76c20
Gabriel Costa Chaves <gccgabr@pm.me>
2025-10-25 18:50:29 -0300

    Atualização do esquema.

--- database/migrations/tarefa.php
+++ database/migrations/tarefa.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('tarefa', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
-            $table->boolean('completo')->unique();
+            $table->boolean('completo');
         });
     }
 
